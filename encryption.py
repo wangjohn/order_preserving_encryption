2a2ae965e5f3c4e75b498d3f699abac3710bcd36
wangjohn <wangjohn@mit.edu>
2013-04-21 15:43:31 -0400

    Created an encryption scheme which is super basic.

---
+++ encryption.py
@@ -0,0 +1,10 @@
+# Super basic encryption scheme.
+class BasicEncryptionScheme:
+    def __init__(self):
+        pass
+
+    def encrypt(self, key, message):
+        return message
+
+    def decrypt(self, key, ciphertext):
+        return message
