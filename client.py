ab6ff50f8cb339982b8092f60d325b2e5356c511
wangjohn <wangjohn@mit.edu>
2013-04-21 17:20:41 -0400

    Added a query method to the client.

--- client.py
+++ client.py
@@ -7,6 +7,11 @@ class Client:
         self.key = self.encryption_scheme.generate_key()
         self.communication_channel = communication_channel
 
+    def query(self, message):
+        ciphertext = self.encryption_scheme.encrypt(self.key, message)
+        client_message = protocol.ClientMessage().query(ciphertext)
+        return self._send_client_message(client_message)
+
     # Stores a plaintext message +message+ in the database by communicating and
     # interacting with the server. The plaintext will be encrypted into a
     # ciphertext, and this ciphertext will be stored at the server in an order
@@ -27,33 +32,37 @@ class Client:
                     # Insert the original ciphertext, using the previous
                     # ciphertext and direction.
                     return self._insert(previous_ciphertext[0], original_ciphertext, previous_ciphertext[1])
+
             elif message < current_ciphertext:
+                # Move left
                 previous_ciphertext = (current_ciphertext, "left")
                 current_ciphertext = self._move_left(current_ciphertext)
+
             elif message > current_ciphertext:
+                # Move right
                 previous_ciphertext = (current_ciphertext, "right")
                 current_ciphertext = self._move_right(current_ciphertext)
+
             else:
                 # Arbitrarily choose to insert in the right direction since the
                 # node values are actually the same.
                 return self._insert(current_node, original_ciphertext, "right")
 
-
     def _get_root(self):
         client_message = protocol.ClientMessage().get_root()
-        self._send_client_message(client_message)
+        return self._send_client_message(client_message)
 
     def _move_left(self, ciphertext):
         client_message = protocol.ClientMessage().move_left(ciphertext)
-        self._send_client_message(client_message)
+        return self._send_client_message(client_message)
 
     def _move_right(self, ciphertext):
         client_message = protocol.ClientMessage().move_right(ciphertext)
-        self._send_client_message(client_message)
+        return self._send_client_message(client_message)
 
     def _insert(self, current_ciphertext, new_ciphertext, direction):
         client_message = protocol.ClientMessage().insert(current_ciphertext, new_ciphertext, direction)
-        self._send_client_message(client_message)
+        return self._send_client_message(client_message)
 
     # Communicate with the server and get back the +ciphertext+ that the
     # server responds with
