1748840bcee036ade2a4fe2e00fbdaba344637b7
Max Kolysh <maxko87@gmail.com>
2013-05-06 18:24:06 -0400

    working on main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from threading import Thread
+import time
 import communication_channel, protocol, client, server
 
 factory = communication_channel.CommunicationFactory()
@@ -6,7 +8,28 @@ client_channel = factory.build_for("client")
 client = client.Client(client_channel)
 server = server.Server(server_channel)
 
-server.run()
-client_message = "010101"
-server_response = client.query(client_message)
-print server_response
+print "initialized"
+
+def client_function(inputs):
+    while True:
+        print "looping client function"
+        if (inputs):
+            print "sending", inputs[0], "to server"
+            client.insert_message(inputs[0])
+            inputs = inputs[1:]
+
+def server_function():
+    while True:
+        print "looping server function"
+        print server.communication_channel._send_queue.qsize()
+        print server.communication_channel._receive_queue.qsize()
+        server.run()
+
+
+inputs = ["0001", "0010", "0100", "1000"]
+
+client_thread = Thread(target=client_function, args=(inputs,))
+server_thread = Thread(target=server_function)
+client_thread.start()
+server_thread.start()
+
